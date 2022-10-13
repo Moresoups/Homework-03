2f553d94bf0c7f2a4cec0561b1ce4190a241734f
Moresop <gardner_christopher@wheatoncollege.edu>
2022-10-12 22:44:52 -0400

    moved script and style sheet out so they work, nothing else still works

--- script.js
+++ script.js
@@ -64,17 +64,15 @@ for (let i = 0; i < numberOfCharacters; i++) {
   finalPassword = finalPassword + possibleCharacters[rng];
 }
 return finalPassword;
-    };
+};
 
 var generateBtn = document.querySelector("#generate");
 
 
-    function writePassword() {
+  function writePassword() {
       var password = generatePassword();
       var passwordText = document.querySelector("#password");
     passwordText.value = password;
     }
    // Add event listener to generate button 
 generateBtn.addEventListener("click", writePassword);
-
-}
